4c17752c6292cb71bccb338967778dbf6ed76859
Creakyinertia <96242141+Creakyinertia@users.noreply.github.com>
2023-10-06 11:55:26 +0530

    Add files via upload

---
+++ Hookcounter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+function Hookcounter() {
+  const [count,setCount]=useState(0)
+  return (
+    <>
+    <div>{count}</div>
+    <button onClick={()=>setCount(count+1)}>increment</button>
+    <button onClick={()=>setCount(0)}>reset</button>
+    <button onClick={()=>setCount(count-2)}>decrement</button>
+    </>
+  )
+}
+
+export default Hookcounter
